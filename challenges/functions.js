267addd760ac65db89c272d36ae51e85c0bfc4fc
SilverMaiden <h.specials@gmail.com>
2019-09-20 09:02:04 -0700

    completed functions/closures

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,4 +1,4 @@
-// ==== Callbacks ====  
+// ==== Callbacks ====
 
 /* Step 1: Create a higher-order function
   * Create a higher-order function named consume with 3 parameters: a, b and cb
@@ -7,26 +7,48 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(a, b, cb) {
+    return cb(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+  * Create a function named multiply that returns the product of two numbers
+  * Create a function named greeting that accepts a first and last name and returns
+  * "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function greeting(first, last) {
+    return `Hello ${first} ${last}, nice to meet you!`;
+
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
-// ==== Closures ==== 
+// ==== Closures ====
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
-
+// Explanation: nestedfunction can access the variable "internal" since
+// closures have access to the scope of the outer functions which they are nested within.
+// Example: myFunction can access "external", even though the variable is declared
+// on a global scope, and not myFunction's local scope.
+// In a way, closures also follow the parent-child structure:
+// Children, aka nested functions can "inherit"/access values
+// from their parents all the way to the global scope,
+// however Parents do not "inherit"/access values from their children.
 
 const external = "I'm outside the function";
 
